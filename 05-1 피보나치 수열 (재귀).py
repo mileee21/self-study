65c510b3de1bcfc3e258eee05d02cb67bb415ca1
mileee21 <81150568+mileee21@users.noreply.github.com>
2021-03-30 23:49:51 +0900

    Update 05-1 피보나치 수열 (재귀).py

--- 05-1 피보나치 수열 (재귀).py
+++ 05-1 피보나치 수열 (재귀).py
@@ -3,18 +3,19 @@
 '''
 def f_num(n):
     
-    if n==1: #조건에 문제가 있었는데 왜 그런걸까...?~!??!?!??!??!?
+    if n==1: #조건에 문제가 있네!!!!!!!
         return n
     else:
-        result= f_num(n-1)+f_num(n-2)
+        result= f_num(n-1)+f_num(n-2) #0, -1, -2 계속 내려가서 ^^^^^^ 무한대^^^
     return result
     
-print(f_num(7))'''
+print(f_num(2))
 
+'''
 #제대로 돌아가는 버전 
 def f_num(n):
     
-    if n<=1: #조건에 문제가 있었는데 왜 그런걸까...?~!??!?!??!??!?
+    if n<=1: 
         return n
     result= f_num(n-1)+f_num(n-2)
     return result
